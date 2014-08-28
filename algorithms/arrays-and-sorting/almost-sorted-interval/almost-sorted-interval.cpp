fc06ed94191c80c496e375976c2f25e99cf1c76b
zeyuanxy <zeyuanxy@gmail.com>
2014-08-28 19:32:23 +0800

    add almost-sorted-interval

--- algorithms/arrays-and-sorting/almost-sorted-interval/almost-sorted-interval.cpp
+++ algorithms/arrays-and-sorting/almost-sorted-interval/almost-sorted-interval.cpp
@@ -1,34 +1,68 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
+int N = 0;
+
+int *bit = NULL;
+
+void add(int x, int v) {
+    while (x <= N) {
+        bit[x] += v;
+        x += (x & -x);
+    }
+}
+
+int ask(int x) {
+    int ret = 0;
+    while (x) {
+        ret += bit[x];
+        x -= (x & -x);
+    }
+    return ret;
+}
+
 int main() {
-    int N;
     cin >> N;
-    int *array = new int[N];
-    for (int i = 0; i < N; ++i) {
+    int *array = new int[N + 1];
+    for (int i = 1; i <= N; ++i) {
         cin >> array[i];
     }
-    int *stack = new int[N];
-    int top = -1;
-    int *left = new int[N];
-    for (int i = 0; i < N; ++i) {
-        while (top >= 0 && a[i] > a[stack[top]])
+    int *stack = new int[N + 1];
+    int top = 0;
+    int *left = new int[N + 1];
+    for (int i = 1; i <= N; ++i) {
+        while (top > 0 && array[i] > array[stack[top - 1]])
             --top;
-        left[i] = (top >= 0)?stack[top]:-1;
-        stack[++top] = i;
+        left[i] = top?stack[top - 1]:0;
+        stack[top++] = i;
     }
-    top = -1;
-    int *right = new int[N];
-    for (int i = N - 1; i >= 0; --i) {
-        while (top >= 0 && a[i] < a[stack[top]])
+    top = 0;
+    int *right = new int[N + 1];
+    for (int i = N; i >= 1; --i) {
+        while (top > 0 && array[i] < array[stack[top - 1]])
             --top;
-        right[i] = (top >= 0)?stack[top]:N;
-        stack[++top] = i;
+        right[i] = top?stack[top - 1]:N + 1;
+        stack[top++] = i;
     }
     long long ans = 0;
-    for (int i = N - 1; i >= 0; --i) {
-        
+    vector<vector<int> > r;
+    for (int i = 0; i <= N; ++i) {
+        vector<int> temp;
+        r.push_back(temp);
     }
+    bit = new int[N + 1];
+    memset(bit, 0, sizeof(int) * (N + 1));
+    for (int L = N; L >= 1; --L) {
+        for (auto & i : r[L]) {
+            add(i, -1);
+        }
+        add(L, 1);
+        r[left[L]].push_back(L);
+        ans += ask(right[L] - 1);
+    }
+    cout << ans << endl;
+    return 0;
 }
