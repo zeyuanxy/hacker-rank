d76b8e653c4c5918c4b7bb3d60a31a6aedfcd97e
zeyuanxy <zeyuanxy@gmail.com>
2015-06-01 02:04:13 +0800

    add whole framework for the-tree-of-life

--- fp/recursion/the-tree-of-life/the-tree-of-life.hs
+++ fp/recursion/the-tree-of-life/the-tree-of-life.hs
@@ -15,17 +15,16 @@ data Tree a
     | Branch (Tree a) a (Tree a)
       deriving (Functor, Show)
       
-type Rule a = a -> a -> a -> a -> a
+getValue :: Tree a -> a
+getValue (Left a) = a
+getValue (Branch _ a _) = a
 
-type Dir = Char
+      
+type Rule a = a -> a -> a -> a -> a
 
 makeRule :: Word16 -> Rule Bool
 makeRule ruleEncoding v1 v2 v3 v4 = testBit ruleEncoding pos
     where pos = sum (ZipWith (*) (map fromEnum [v4, v3, v2, v1]) (iterate (*2 ) 1))
-    
-getValue :: Tree a -> a
-getValue (Left v) = v
-getValue (Branch _ v _) = v
 
 nextTree :: Rule Bool -> Tree Bool -> Tree Bool
 nextTree rule = withParent False
@@ -33,11 +32,35 @@ nextTree rule = withParent False
         Leaf a -> Leaf (rule p False v False)
         Branch l a r -> Branch (withParent a l)
                                (rule p (getValue l) a (getValue r))
-                               (withParent a r)
-                               
+                               (withParent a r) 
+
 parseTree :: Parser (Tree Bool)
 parseTree = parseBranch <|> parseLeaf
     where parseCell = (char 'X' >> return True) <|> (char '.' >> return False)
           parseLeaf = Leaf <$> parseCell
           parseBranch = between (char '(') (char ')')
                                 (Branch <$> parseTree <*> (space *> parseCell *< space) <*> parseTree)
+                                
+buildTrees :: Word16 -> Tree Bool -> [Tree Bool]
+buildTrees i = iterate (nextTree (makeRule i))
+
+
+type Dir = Char
+
+parseQuery :: String -> (Int, [Dir])
+parseQuery s = (read si, path)
+    where (si:sp:_) = words s
+          path = init . tail $ sp
+
+
+main :: IO ()
+main = do
+    trees <- buildTrees
+                <$> readLn
+                <*> ((parse (parseTree <* (spaces >> eof))) <$> getLine)
+    n <- readLn
+    evalStateT (replicateM_ n (do
+                                (i, ps) <- lift (parseQuery <$> getLine)
+                                
+                                ))
+               (trees, [])
