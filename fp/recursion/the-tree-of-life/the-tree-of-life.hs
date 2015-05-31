6d2d5bf88d5b39018fec27651892fe3e2b656582
zeyuanxy <zeyuanxy@gmail.com>
2015-06-01 01:40:45 +0800

    add parse and rule in the-tree-of-life

--- fp/recursion/the-tree-of-life/the-tree-of-life.hs
+++ fp/recursion/the-tree-of-life/the-tree-of-life.hs
@@ -26,3 +26,18 @@ makeRule ruleEncoding v1 v2 v3 v4 = testBit ruleEncoding pos
 getValue :: Tree a -> a
 getValue (Left v) = v
 getValue (Branch _ v _) = v
+
+nextTree :: Rule Bool -> Tree Bool -> Tree Bool
+nextTree rule = withParent False
+    where withParent p t = case t of
+        Leaf a -> Leaf (rule p False v False)
+        Branch l a r -> Branch (withParent a l)
+                               (rule p (getValue l) a (getValue r))
+                               (withParent a r)
+                               
+parseTree :: Parser (Tree Bool)
+parseTree = parseBranch <|> parseLeaf
+    where parseCell = (char 'X' >> return True) <|> (char '.' >> return False)
+          parseLeaf = Leaf <$> parseCell
+          parseBranch = between (char '(') (char ')')
+                                (Branch <$> parseTree <*> (space *> parseCell *< space) <*> parseTree)
