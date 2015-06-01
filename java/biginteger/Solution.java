f8905b09131003fa3fa02907daec12fa16cb88b8
zeyuanxy <zeyuanxy@gmail.com>
2015-06-01 12:11:46 +0800

    add java-big-integer

---
+++ java/biginteger/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger a = in.nextBigInteger();
+        BigInteger b = in.nextBigInteger();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
