89c30488c67f9481fd624022487b39ed11f6b225
zeyuanxy <zeyuanxy@gmail.com>
2015-09-15 17:44:59 -0400

    update bash-tutorials---compute-the-average

--- shell/bash/bash-tutorials---compute-the-average/bash-tutorials---compute-the-average.sh
+++ shell/bash/bash-tutorials---compute-the-average/bash-tutorials---compute-the-average.sh
@@ -2,11 +2,7 @@ d x
 sum=0
 for ((i = 0; i < $x; ++i))
 do
-      read a;
-          sum=$(($sum+$a))
-        done
-        if ((x == 300)); then
-              echo "4696.947"
-            else 
-                  echo "scale=3; ($sum*10000)/($x*10000) " | bc -l
-                fi
+    read a;
+    sum=$(($sum+$a))
+done
+printf "%.3f\n" `echo "$sum / $N" | bc -l`
