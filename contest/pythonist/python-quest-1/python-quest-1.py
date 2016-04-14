81bf67ff83acde6cd880492881a721609f214637
Zeyuan Shang <zeyuanxy@gmail.com>
2016-04-14 21:15:02 +0800

    add python-quest-1

---
+++ contest/pythonist/python-quest-1/python-quest-1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: Zeyuan Shang
+# @Date:   2016-04-14 21:14:42
+# @Last Modified by:   Zeyuan Shang
+# @Last Modified time: 2016-04-14 21:14:53
+for i in range(1,input()): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print (10 ** i - 1) / 9 * i
