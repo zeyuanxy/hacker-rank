e75d8f5b8bfc9739f22769d0913a0ad0c0369554
Zeyuan Shang <zeyuan.shang@hulu.com>
2016-05-04 13:20:32 +0800

    add swap-case

---
+++ contest/pythonist2/swap-case/swap-case.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: Zeyuan Shang
+# @Date:   2016-05-04 13:19:30
+# @Last Modified by:   Zeyuan Shang
+# @Last Modified time: 2016-05-04 13:19:43
+import string
+print string.swapcase(raw_input())
