8e58d50f7c4747cef045b20f3cda433e5179f216
Zeyuan Shang <zeyuan.shang@hulu.com>
2016-05-06 15:36:20 +0800

    add capitalize

---
+++ contest/pythonist3/capitalize/capitalize.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Author: Zeyuan Shang
+# @Date:   2016-05-06 15:36:04
+# @Last Modified by:   Zeyuan Shang
+# @Last Modified time: 2016-05-06 15:36:08
+print ' '.join(map(lambda x: x.capitalize(), raw_input().split(' ')))
